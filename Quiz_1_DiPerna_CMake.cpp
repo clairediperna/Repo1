8040a0647a26be3ed7211c5f0f9405d817494a3a
clairediperna <143756176+clairediperna@users.noreply.github.com>
2023-08-31 17:05:57 -0400

    Add files via upload

---
+++ Quiz_1_DiPerna_CMake.cpp
@@ -0,0 +1,11 @@
+﻿// Quiz_1_DiPerna_CMake.cpp : Defines the entry point for the application.
+//
+
+#include "Quiz_1_DiPerna_CMake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Claire DiPerna" << endl;
+}
